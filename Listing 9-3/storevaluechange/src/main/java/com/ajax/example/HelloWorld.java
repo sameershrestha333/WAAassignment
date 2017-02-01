32e284d9d7546e0a66ef6ef104ab757455db1bab
sameershrestha333 <sameershrestha333@gmail.com>
2017-02-01 14:50:22 -0600

    changes on validation

--- Listing 9-3/storevaluechange/src/main/java/com/ajax/example/HelloWorld.java
+++ Listing 9-3/storevaluechange/src/main/java/com/ajax/example/HelloWorld.java
@@ -1,13 +1,25 @@
 package com.ajax.example;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 @ManagedBean(name = "helloWorld")
 @RequestScoped
 public class HelloWorld {
 	private String userName;
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public String getUserName() {
 		return userName;
@@ -25,4 +37,22 @@ public class HelloWorld {
 		}
 	}
 
+	public void validateName(FacesContext fc, UIComponent c, Object value) {
+		if (((String) value).contains("_")) {
+
+			throw new ValidatorException(new FacesMessage("Name cannot contain underscores)"));
+
+		}
+		if (((String) value).equals("")) {
+
+			throw new ValidatorException(new FacesMessage("Name cannot be empty)"));
+
+		}
+
+		if (((String) value).equals("admin")) {
+
+			throw new ValidatorException(new FacesMessage("Such username already exits!)"));
+
+		}
+	}
 }
